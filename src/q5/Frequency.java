c429a6632d0c28dc3ba66018681441f202445804
Brent Atchison <atchisonbrent@gmail.com>
2018-09-14 02:13:03 -0500

    Object organization

--- src/q5/Frequency.java
+++ src/q5/Frequency.java
@@ -6,30 +6,38 @@ public class Frequency implements Callable<Integer> {
     public static int parallelFreq(int x, int[] A, int numThreads) {
     	try {
     		
-    		int ret = 0;
-    		for (int i = 0; i < A.length; i++) {
-    			if (x == A[i]) {
-    				ret++;
-    			}
-    		}
-    		
-    		return ret;
+    		/* Sequential Solution */
+//    		int ret = 0;
+//    		for (int i = 0; i < A.length; i++) {
+//    			if (x == A[i]) {
+//    				ret++;
+//    			}
+//    		}
+//    		
+//    		return ret;
     		
     		/* Calculate work per thread */
-//    		int work = A.length / numThreads;
-//    		int remainder = A.length % numThreads;
-//    		
-//    		/* Start threads in loop */
-//    		for (int i = 0; i < numThreads; i++) {
-//    			Thread t = new Thread();
-//    			t.start();
-//    			return 0;
-//    		}
+    		int work = A.length / numThreads;
+    		int remainder = A.length % numThreads;
+    		
+    		/* Start threads in loop */
+    		for (int i = 0; i < numThreads; i++) {
+    			Frequency t = new Frequency();
+    			t.start();
+    			return 0;
+    		}
+    		
+    		return 0;
     	}
     	catch (Exception e) {
     		return -1;	// Return -1 if invalid input
     	}
     }
+
+	private void start() {
+		// TODO Auto-generated method stub
+		
+	}
 
 	@Override
 	public Integer call() throws Exception {

07bb3f9ac42c885b239b5b3ecc10c56b9e73f534
atchisonbrent <atchisonbrent@gmail.com>
2018-09-14 01:12:11 -0500

    Seq working

--- src/q5/Frequency.java
+++ src/q5/Frequency.java
@@ -6,27 +6,29 @@ public class Frequency implements Callable<Integer> {
     public static int parallelFreq(int x, int[] A, int numThreads) {
     	try {
     		
-    		/* Calculate work per thread */
-    		int work = A.length / numThreads;
-    		int remainder = A.length % numThreads;
-    		
-    		/* Start threads in loop */
-    		for (int i = 0; i < numThreads; i++) {
-    			Thread t = new Thread(new Runnable() {
-    				
-    				@Override
-    				public void run() {
-    					
-    				}
-    			});
-    			t.start();
-    			return 0;
+    		int ret = 0;
+    		for (int i = 0; i < A.length; i++) {
+    			if (x == A[i]) {
+    				ret++;
+    			}
     		}
+    		
+    		return ret;
+    		
+    		/* Calculate work per thread */
+//    		int work = A.length / numThreads;
+//    		int remainder = A.length % numThreads;
+//    		
+//    		/* Start threads in loop */
+//    		for (int i = 0; i < numThreads; i++) {
+//    			Thread t = new Thread();
+//    			t.start();
+//    			return 0;
+//    		}
     	}
     	catch (Exception e) {
     		return -1;	// Return -1 if invalid input
     	}
-		return 0;
     }
 
 	@Override
